f89d4ad54804483dbab766431c6b88ced93b2315
kavikannavkl <kavikannavkl@gmail.com>
2024-07-10 21:36:31 +0530

    Updated

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './EventList.css';
 
 const events = [
-  { id: 1, title: 'Event 1', image: 'path/to/event1.jpg' },
-  { id: 2, title: 'Event 2', image: 'path/to/event2.jpg' },
-  { id: 3, title: 'Event 3', image: 'path/to/event3.jpg' },
+  { id: 1, title: 'Event 1', image: 'https://marketplace.canva.com/EAFIygYzkes/1/0/1131w/canva-blue-minimalist-concert-music-cover-poster-CGNgQz4KqL0.jpg' },
+  { id: 2, title: 'Event 2', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStigdTJG-yYAYLuFwCS9p_ARvFIKagoBC_7w&s' },
+  { id: 3, title: 'Event 3', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFbTBTe_fucP6gpumnu2UIEkEpyPiO5OBAow&s' },
 ];
 
 const EventsPage = () => {
--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,109 +1,31 @@
-// // // import React from 'react';
-// // // import { useParams, Link } from 'react-router-dom';
-// // // import './MovieDetail.css';
-
-// // // const MovieDetail = () => {
-// // //   const { id } = useParams();
-// // //   const movie = { title: `Movie ${id}`, description: `Description for movie ${id}` };
-
-// // //   return (
-// // //     <div className="movie-detail">
-// // //       <h1>{movie.title}</h1>
-// // //       <p>{movie.description}</p>
-// // //       <Link to={`/book/${id}`}>Book Now</Link>
-// // //     </div>
-// // //   );
-// // // };
-
-// // // export default MovieDetail;
-// // // MovieDetail.js
-
-// // import React from 'react';
-// // import { useParams, Link } from 'react-router-dom';
-// // import './MovieDetail.css';
-
-// // const MovieDetail = () => {
-// //   const { id } = useParams();
-// //   const movie = { title: `Movie ${id}`, description: `Description for movie ${id}` };
-
-// //   return (
-// //     <div className="movie-detail">
-// //       <div className="movie-info">
-// //         <h1>{movie.title}</h1>
-// //         <p>{movie.description}</p>
-// //       </div>
-// //       <div className="book-now">
-// //         <Link to={`/book/${id}`}>Book Now</Link>
-// //       </div>
-// //     </div>
-// //   );
-// // };
-
-// // export default MovieDetail;
-// // MovieDetail.js
-
-// import React from 'react';
-// import { useParams, Link } from 'react-router-dom';
-// import './MovieDetail.css';
-
-// const MovieDetail = () => {
-//   const { id } = useParams();
-//   const movie = { title: `Movie ${id}`, description: `Description for movie ${id}` };
-
-//   return (
-//     <div className="movie-detail">
-//       <div className="movie-info">
-//         <div className="title">
-//           <h1>{movie.title}</h1>
-//         </div>
-//         <div className="Box">
-//         <div className="description">
-        
-//           <p>{movie.description}</p>
-//         </div>
-//       <div className="book-now">
-//         <Link to={`/book/${id}`}>Book Now</Link>
-//       </div>
-//       </div>
-//       </div>
-      
-//     </div>
-//   );
-// };
-
-// export default MovieDetail;
-// MovieDetail.js
-
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 import './MovieDetail.css';
 
 const MovieDetail = () => {
   const { id } = useParams();
-  const movie = { 
-    title: `Movie ${id}`, 
-    description: `Description for movie ${id}`, 
-    imageUrl: `https://static.vecteezy.com/system/resources/previews/011/860/693/non_2x/its-movie-time-vector.jpg`,  // Example image URL
-  };
-//   https://via.placeholder.com/300
+  const movies = [
+    { id: 1, title: 'Kalki', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', imageUrl: 'https://cdn.123telugu.com/content/wp-content/uploads/2024/04/Kalki-2898-AD-1.jpg' },
+    { id: 2, title: 'Garudan', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', imageUrl: 'https://assetscdn1.paytm.com/images/cinema/Garudan-05x750-74275de0-1bf1-11ef-bee8-8b90ac5cda97.jpg?format=webp&imwidth=322' },
+    { id: 3, title: 'Maharaja', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', imageUrl: 'https://assetscdn1.paytm.com/images/cinema/Garudan-05x750-74275de0-1bf1-11ef-bee8-8b90ac5cda97.jpg?format=webp&imwidth=322', imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTE0ZjY3MTUtYzUwMy00ZWEzLTgwMTYtMjg3ZDE1NTMxOTE4XkEyXkFqcGc@._V1_.jpg' },
+  ];
+  const movie = movies.find(movie => movie.id === parseInt(id));
 
   return (
     <div className="movie-detail">
       <div className="movie-info">
-        <div className="title">
-          <h1>{movie.title}</h1>
-        </div>
+       
         <div className="Box">
           <div className="card">
             <div className="image">
               <img src={movie.imageUrl} alt={movie.title} />
             </div>
-            <div className="book-now">
-              <Link to={`/book/${id}`} className="btn-book-now">Book Now</Link>
+            <div className="descript">
+              <p>{movie.description}</p>
+              <div className="book-now">
+                <Link to={`/book/${id}`} className="btn-book-now">Book Now</Link>
+              </div>
             </div>
-          </div>
-          <div className="description">
-            <p>{movie.description}</p>
           </div>
         </div>
       </div>
